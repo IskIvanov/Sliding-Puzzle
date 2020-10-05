e234f1e14e5d55e90966d5c5a6e628f25bbfc2f0
Iskren Ivanov <iskren@stakelogic.com>
2020-10-05 13:17:15 +0200

    Cleanup app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React,{ useState } from 'react';
 import Puzzle from './components/Puzzle'
+import { Input } from '@material-ui/core';
 
 export default function App() {
-	const [counter, setCounter] = useState(0);
-	// Create a stete and set it to the state of the puzzle component
-		// Every time when component renders add one to state 
+
   return (
     <div className="App">
       <header className="App-header">
        	<Puzzle />
+		<Input/>
       </header>
     </div>
   );
